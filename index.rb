015e630070557a909567159a3da54f40a2ae684d
Anna Farman <anna.farman@gmail.com>
2024-02-21 23:38:00 -0500

    Complete TOP: TicTacToe - Ruby basics

--- index.rb
+++ index.rb
@@ -1,62 +1,109 @@
 board = Array.new(3) {Array.new(3, ' ')}
-
+used_index = []
+playerX = true
+no_winner = true
 
 def init_board(board)
     board.each_with_index do |row, row_index|
         row.each_with_index do |column, column_index|
-            print "#{row_index}#{column_index} | ".chomp
+            print "#{row_index}#{column_index} | " 
         end
         puts "\n"
     end 
 end
 
-def update_board(index)
+def current_board(board, index, playerX)
     x = index[0].to_i
     y = index[1].to_i
-    puts x
-    puts y
+    board[x][y] = playerX ? "X" : "O"
+    draw_board(board)
 end
 
-def check_player_input(input)
-    allowed_index = [00,01,02,10,11,12,20,21,22]
-    if allowed_index.include?(input)
-        #check if its already been used
-    else
-        false
-    end
+def draw_board(board)
+    board.each do |row|
+        row.each do |column|
+            print "#{column} | "
+        end
+        puts "\n"
+    end 
+    puts "\n"
 end
 
-def get_index(player)
-    puts "#{player}, please enter your location: "
-    index = gets.chomp
-    index
+def check_player_input(input, used_index)
+    allowed_index = %w[00 01 02 10 11 12 20 21 22]
+    !used_index.include?(input) && allowed_index.include?(input)
 end
 
-def play_game(board)
-    any_winner = false
-    playerX = true
-    used_index = []
-    puts "Here is the location index of the board: "
-    puts init_board(board)
+def get_index(playerX)
+    print playerX ? "Player X, please enter index: " : "Player O, please enter index: "
+    gets.chomp
+end
 
-    while !any_winner
-        if(playerX)
-            index = get_index("Player XX")
-            can_continue = check_player_input(index, used_index)
-            if can_continue
-                playerX = false
-            else
-                puts "Please put a valid index number."
-                puts init_board(board)
+def check_winner(board, no_winner)
+    winning_combinations = [
+        ['00', '01', '02'], ['10', '11', '12'], ['20', '21', '22'],   # Rows
+        ['00', '10', '20'], ['01', '11', '21'], ['02', '12', '22'],   # Columns
+        ['00', '11', '22'], ['20', '11', '02']                  # Diagonals
+      ]
+    
+    winner = nil
+    players = ["X", "O"]
+
+    players.each do |player|
+        player_locations = []
+
+        board.each_with_index do |row, row_index|
+            row.each_with_index do |cell, column_index|
+                if cell == player
+                    player_locations << "#{row_index}#{column_index}"
+                end
             end
-        else
-            player_turn("Player OO")
-            playerX = true
+        end
+        # p player_locations
+
+        winning_combinations.each do |sets|
+            if sets.all? { |element| player_locations.include?(element) }
+                winner = player
+                no_winner = false
+                break 
+            end
         end
     end
+    # p   [winner, no_winner]
+    [winner, no_winner]
+
 end
 
-play_game(board)
+
+def play_game(board, used_index, playerX, no_winner)
+    puts "Here is the location index of the board: "
+    init_board(board)
+
+    winner_info = []
+
+    while used_index.length < 9 
+        index = get_index(playerX)
+        if !check_player_input(index, used_index)
+            puts "Please enter a valid index number."
+            init_board(board)
+        else
+            used_index << index
+            current_board(board, index, playerX)
+            playerX = !playerX
+        end
+        winner_info = check_winner(board, no_winner)
+
+        if !winner_info[1]
+            puts "Congrats the winner is Player #{winner_info[0]}"
+            break
+        end
+    end
+
+    if winner_info[1] then puts "There is no winner."
+    end
+end
+
+play_game(board, used_index, playerX, no_winner)
 
 
 
